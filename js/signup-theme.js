adf01e4de70e7ff6a3d07f31aa535dbbdb05b9de
edison <edison@Administrators-MacBook-Pro-3.local>
2015-06-11 10:36:18 -0400

    from master

--- js/signup-theme.js
+++ js/signup-theme.js
@@ -6,7 +6,7 @@ $(function () {
 		validClass: "success",
 		errorClass: "error",
 		errorElement:"em",
-		submitHandler: function() { alert("Submitted!") },
+		submitHandler: function() { form.submit(); },
 		success: function(label, element){
 			// console.log( "test2: ", label, element );
 			var _this = $(element).parent();
