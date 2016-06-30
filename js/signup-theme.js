a7eadf880708a1806bdbd92e33806af25c509836
edison <edison@cmarino.mshanken.com>
2016-06-30 11:38:19 -0400

    new newsleter have been added as stand alone page

--- js/signup-theme.js
+++ js/signup-theme.js
@@ -1,4 +1,4 @@
-function countUnchecked() {
+    function countUnchecked() {
   var n = $( ".row-two input[type=checkbox]:checked" ).length; // true if 0 checked
   var t = $( "input[type=checkbox]#2562328995:checked" ).length; // true if 0 checked
   console.log(n + (n == 1 ? " is" : " are") + " checked!");
@@ -18,8 +18,8 @@ function countUnchecked() {
 		}
 	}
 }
-function populate() { 
-	var checkboxes = document.getElementsByName('ci-custom3'); 
+function populate() {
+	var checkboxes = document.getElementsByName('ci-custom3');
 	var ip1 = document.getElementById('ci-custom3');
 
 	ip1.value = '';
@@ -27,19 +27,19 @@ function populate() {
 	for (var i = 0, iLen = checkboxes.length; i < iLen; i++) {
 
 		if (checkboxes[i].checked) {
-			if (ip1.value.length === 0) { 
-				ip1.value = checkboxes[i].value; 
-			} else { 
-				ip1.value = ip1.value + ',' + checkboxes[i].value; 
-			} 
-		} 
+			if (ip1.value.length === 0) {
+				ip1.value = checkboxes[i].value;
+			} else {
+				ip1.value = ip1.value + ',' + checkboxes[i].value;
+			}
+		}
 	}
-} 
+}
 $(function () {
 	$("#CI_subscribeForm").validate({
 		rules: {
 			CI_email:{ email:true },
-			CI_LID: { required: true }, 
+			CI_LID: { required: true },
 			/* ,CI_custom3: { required: countUnchecked }
 			"#2562328995": { required: true } */
 		},
@@ -58,7 +58,7 @@ $(function () {
 				$(_this).removeClass("has-error").addClass("has-success");
 				$(_this).find("span.glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-ok");
 				$(_this).find("span.sr-only").attr("id", "inputSuccess2Status").text("(success)");
-			}	
+			}
 		},
 		invalidHandler: function(event, validator){
 			// console.log("test:", validator, event, validator.invalid);
@@ -91,5 +91,41 @@ $(function () {
 	      this.checked = false;
 	    }
 	  }
-	})/*  */
+	})
+    /* For Private Guide o Dining */
+    $("#CI_subscribeForm_pg2d").validate({
+        rules: {
+            CI_email:{ email:true },
+            /* CI_LID: { required: true },
+            ,CI_custom3: { required: countUnchecked }
+            "#2562328995": { required: true } */
+        },
+        messages: {
+            // CI_LID: "Please pick at least one from the list below",
+            // CI_custom3: "Please select your wine interests for the Ratings Flash newsletter."
+        },
+        validClass: "success",
+        errorClass: "error",
+        errorElement:"em",
+        submitHandler: function(form) { form.submit(); },
+        success: function(label, element){
+            // console.log( "test2: ", label, element );
+            var _this = $(element).parent();
+            if( $(_this).hasClass("has-error") ){
+                $(_this).removeClass("has-error").addClass("has-success");
+                $(_this).find("span.glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-ok");
+                $(_this).find("span.sr-only").attr("id", "inputSuccess2Status").text("(success)");
+            }
+        },
+        invalidHandler: function(event, validator){
+            // console.log("test:", validator, event, validator.invalid);
+            $.each( validator.invalid, function( key, value ) {
+                // console.log( key + ": " + value );
+                var _this = $("#"+key).parent();
+                $(_this).addClass("has-success");
+                $(_this).addClass("has-error has-feedback");
+                $(_this).find(".form-control-feedback").css("display","block");
+            });
+        }
+    });
 });
